aee60eefa30009c4503c1e772a36d7e67a639955
Putra Harapan Tafonao <putraharapantafonao199@gmail.com>
2026-07-30 22:50:52 +0700

    Final wrap-up and dynamic IP routing fixes

--- od_frontend/lib/services/order_service.dart
+++ od_frontend/lib/services/order_service.dart
@@ -1,9 +1,15 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:io' show Platform;
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 class OrderService {
-  static const String baseUrl = 'http://10.0.2.2:8000/api';
+  static String get baseUrl {
+    if (kIsWeb) return 'http://127.0.0.1:8000/api';
+    if (Platform.isAndroid) return 'http://10.0.2.2:8000/api';
+    return 'http://127.0.0.1:8000/api';
+  }
 
   Future<Map<String, dynamic>> createOrder({
     required String serviceType,
